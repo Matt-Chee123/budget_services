7d28509b985940e94504bce642eef0ca8cab01ba
Matt-Chee123 <cheematthew10@gmail.com>
2025-01-14 22:22:25 +0000

    added users model and registered

--- users/models.py
+++ users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+import uuid
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    user_id = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
+    nationality = models.CharField(max_length=100, blank=True, null=True)
+
