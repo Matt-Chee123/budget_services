9a1f96d9edf4c61300d67ca945499386c46e8f5d
Matt-Chee123 <cheematthew10@gmail.com>
2025-01-29 22:01:18 +0000

    stuff

--- tracker/models.py
+++ tracker/models.py
@@ -1,16 +1,14 @@
-from tkinter.constants import CASCADE
 from django.db import models
-from users.models import UserProfile
+from django.conf import settings
 import uuid
 
-# Create your models here.
 class Income(models.Model):
     income_id = models.UUIDField(default=uuid.uuid4, primary_key=True, unique=True, editable=False)
     income_name = models.CharField(max_length=40)
     time_frame = models.CharField(max_length=10)
     income_type = models.CharField(max_length=10)
     income_amount = models.IntegerField()
-    user = models.ForeignKey(to=UserProfile,on_delete=models.CASCADE,null=False)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'user_income'
--- users/models.py
+++ users/models.py
@@ -5,5 +5,6 @@ from django.contrib.auth.models import AbstractUser
 class UserProfile(AbstractUser):
     user_id = models.UUIDField(default=uuid.uuid4, primary_key=True, unique=True, editable=False)
     nationality = models.CharField(max_length=100, blank=True, null=True)
+
     class Meta:
         db_table = "users_profiles"
