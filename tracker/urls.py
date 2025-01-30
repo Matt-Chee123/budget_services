4476cd093a9a73a6da093ea84e6c31f6a27b32af
Matt-Chee123 <cheematthew10@gmail.com>
2025-01-30 21:15:29 +0000

    added total income view

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import IncomeView
+from .views import IncomeView, TotalIncomeView
 
 urlpatterns = [
     path('record/', IncomeView.as_view(), name='record'),
+    path('total/',TotalIncomeView.as_view(),name='total'),
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -6,11 +6,25 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Income
 from .serializers import IncomeSerializer
+from django.db.models import Sum
 from users.auth import CustomJWTAuthentication
 
 
 
 # Create your views here.
+class TotalIncomeView(APIView):
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [CustomJWTAuthentication]
+
+    def get(self,request):
+        user_id = request.user.user_id
+        incomes = Income.objects.filter(user=request.user)
+
+        total_income = incomes.aggregate(total=Sum('income_amount'))['total'] or 0
+
+        return Response({
+            'total_income': total_income
+        }, status=status.HTTP_200_OK)
 
 class IncomeView(APIView):
     permission_classes = [IsAuthenticated]
