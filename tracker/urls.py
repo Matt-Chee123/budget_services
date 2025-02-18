c510c61810de20e872d3d997e41baf553a184d70
Matt-Chee123 <cheematthew10@gmail.com>
2025-02-18 21:09:26 +0000

    fix issues

--- tracker/urls.py
+++ tracker/urls.py
@@ -4,5 +4,5 @@ from .views import IncomeView, TotalIncomeView, TaxView
 urlpatterns = [
     path('record/', IncomeView.as_view(), name='record'),
     path('total/',TotalIncomeView.as_view(),name='total'),
-    path('taxRates/', TaxView.as_view, name='tax'),
+    path('taxRates/', TaxView.as_view(), name='tax'),
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -5,7 +5,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Income, Tax
-from .serializers import IncomeSerializer
+from .serializers import IncomeSerializer, TaxSerializer
 from django.db.models import Sum
 from users.auth import CustomJWTAuthentication
 
@@ -46,4 +46,4 @@ class TaxView(APIView):
         tax_rates = Tax.objects
 
         serializer = TaxSerializer(tax_rates,many=True)
-        return Response(serializer.data,status=HTTP_200_OK)
+        return Response(serializer.data,status=status.HTTP_200_OK)
