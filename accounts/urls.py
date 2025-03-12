0be3590e3837f7f041e4f5d38b9b2abacf3ecf21
Matt-Chee123 <cheematthew10@gmail.com>
2025-03-12 21:16:34 +0000

    add path

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import UserAccountsView
+from .views import UserAccountsView, InvestmentsView
 
 urlpatterns = [
     path('all/', UserAccountsView.as_view(), name='accounts'),
+    path('investments/', InvestmentsView.as_view(),name='investments')
 ]
